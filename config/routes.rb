561053a2b4f82f99ddcbddd488cfea0f5b0a0fa2
明沙 <mingsha@mingshadeMacBook-Pro.local>
2017-04-19 23:57:49 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  namespace :admin do
-    resources :jobs
-  end
-
   resources :jobs
   root 'jobs#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
+  end
 end
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
